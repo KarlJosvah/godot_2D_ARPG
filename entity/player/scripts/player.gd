e19d1893b4dbbdd0b184f02b499ca1ec578783f2
KarlJosvah <60389631+KarlJosvah@users.noreply.github.com>
2026-08-05 21:00:02 +0300

    Sword Attack

--- entity/player/scripts/player.gd
+++ entity/player/scripts/player.gd
@@ -1,13 +1,20 @@
 extends CharacterBody2D;
+class_name Player;
 
 @onready var animation_tree: AnimationTree = $AnimationTree;
 @onready var playback : AnimationNodeStateMachinePlayback = animation_tree.get("parameters/StateMachine/playback") as AnimationNodeStateMachinePlayback;
+
+@onready var collision_polygon_2d: CollisionPolygon2D = $HitBoxPivot/Area2D/CollisionPolygon2D;
+@onready var hit_box_pivot: Node2D = $HitBoxPivot;
 
 const SPEED : float = 100.0;
 const ROLL_SPEED : float = 125.0;
 
 var input_vector : Vector2 = Vector2.ZERO;
 var last_input_vector : Vector2 = Vector2.ZERO;
+
+func _ready() -> void:
+	self.collision_polygon_2d.disabled = true;
 
 func _physics_process(_delta: float) -> void:
 	var state = self.playback.get_current_node();
@@ -25,6 +32,7 @@ func _move_state() -> void:
 	if self.input_vector != Vector2.ZERO:
 		self.last_input_vector = self.input_vector;
 		self._update_blend_position(self.input_vector);
+		self._update_hit_box();
 	
 	if Input.is_action_just_pressed("attack"):
 		self.playback.travel("Attack_State");
@@ -46,3 +54,6 @@ func _update_blend_position(direction : Vector2) -> void:
 	self.animation_tree.set("parameters/StateMachine/Move_State/Run_State/blend_position", direction);
 	self.animation_tree.set("parameters/StateMachine/Attack_State/blend_position", direction);
 	self.animation_tree.set("parameters/StateMachine/Roll_State/blend_position", direction);
+
+func _update_hit_box() -> void:
+	self.hit_box_pivot.rotation = self.last_input_vector.angle() - (PI / 2);
--- world/props/grass.gd
+++ world/props/grass.gd
@@ -2,3 +2,8 @@ extends Node2D;
 
 func _ready() -> void:
 	pass;
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	if area.owner is Player:
+		queue_free();
